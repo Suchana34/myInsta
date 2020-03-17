9066c87a0c2543c42112ef57502a0c24274755c4
suchana <suchanachakraborty@gmail.com>
2020-03-17 15:21:53 +0530

    upload section

--- components/upload.js
+++ components/upload.js
@@ -2,7 +2,40 @@ import React from 'react';
 import {FlatList, StyleSheet, Text, View, Image} from 'react-native';
 
 class Upload extends React.Component{
-    
+    constructor(props){
+        super(props);
+
+        this.state = {
+            loggedIn: false,
+
+        };
+    }
+    componentDidMount = () => {
+        var that = this;
+        f.auth().onAuthStateChanged(function(user){
+            if(user){
+                that.setState({
+                    loggedIn: true,
+                });
+            }
+            else{
+                that.setState({
+                    loggedIn: false,
+                });
+            }
+        });
+    }
+    render(){
+        return(
+            <View>
+            {this.state.loggedIn == true ? (
+                <Text>Upload</Text>
+            ):(
+                <Text>Login first</Text>
+            )}
+            </View>
+        )
+    }
 }
 
 export default Upload;
