c518e9b0643c407630a98842fe51eaa563ebfc53
suchana <suchanachakraborty@gmail.com>
2020-03-17 11:25:01 +0530

    modified

--- config/config.js
+++ config/config.js
@@ -2,11 +2,13 @@ import firebase from 'firebase';
 
 var firebaseConfig = {
     //initalise your own configuration from firebase
+
+
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
 
   export const f = firebase;
-  export const data = firebase.database();
+  export const database = firebase.database();
   export const auth = firebase.auth();
   export const storage = firebase.storage();
