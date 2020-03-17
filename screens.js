6315c0003a8e96613c621f7dd2fa3b69fc9eb219
suchana <suchanachakraborty@gmail.com>
2020-03-17 15:18:56 +0530

    Added more screen

--- screens.js
+++ screens.js
@@ -1,8 +1,12 @@
 import React from 'react';
-import {createBottomTabNavigator} from 'react-navigation';
+import {createBottomTabNavigator, createStackNavigator} from 'react-navigation';
 import Feed from './components/feed';
 import Upload from './components/upload';
 import Profile from './components/profile';
+import userProfile from './components/userProfile';
+import comments from './components/comments';
+
+import {auth} from './config/config';
 
 import {
   View,
@@ -12,7 +16,7 @@ import {
   Text,
 } from 'react-native';
 
-const MainStack = createBottomTabNavigator(
+const TabStack = createBottomTabNavigator(
   {
     Feed: {screen: Feed},
     Upload: {screen: Upload},
@@ -20,7 +24,38 @@ const MainStack = createBottomTabNavigator(
   }
 );
 
+const MainStack = createStackNavigator(
+  {
+    Home: {screen: TabStack},
+    User: {screen: userProfile},
+    Comments: {screen: comments}
+  },
+
+  {
+    initialRouteName: 'Home',
+    mode: 'modal',
+
+  }
+);
+
 export default class screen extends React.Component{
+
+  login = async() => {
+    //force user to log in
+    try{
+      let user = await auth.signInWithEmailAndPassword('test@gmail.com', 'password');
+    }
+    catch(error){
+      console.log(error);
+    }
+  }
+
+  constructor(props){
+    super(props)
+
+    this.login();
+  }
+
   render(){
     return(
       <div>
